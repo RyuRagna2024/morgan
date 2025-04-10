b44acace779a4ae99ebae3e66571680b4da81417
RyuRagna2024 <carstensmorganlee@gmail.com>
2025-04-10 14:12:17 +0200

    Created Navbar and Footer

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+// app/(public)/layout.tsx
+
+import Footer from "../_components/Footer";
+import Navbar from "../_components/Navbar";
+
+export default function PublicLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
